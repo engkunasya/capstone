2af9648be92841fc9c4e066c523cd21175c1ef34
engkunasya <engkunasya@gmail.com>
2024-07-07 18:31:52 +0800

    before-jog

--- services.py
+++ services.py
@@ -2,50 +2,52 @@ import os
 
 
 #===== FOR HOTEL =====
-def create_file(filename):
-    if not os.path.exists(filename):
+def create_file_hotel(filename_hotel):
+    if not os.path.exists(filename_hotel):
         try:
-            with open(filename, "x") as file:
-                file.write("hotel | hotel_id | Price | Total\n")
+            with open(filename_hotel, "x") as file:
+                file.write("hotel | hotel_id | hotel_Price | Total Commission\n")
         except IOError as e:
             print(f"Error creating file: {e}")
     else:
-        print(f"File '{filename}' already exists.")
+        print(f"File '{filename_hotel}' already exists.")
 
-def add_hotel(filename):
-    if os.path.exists(filename):
+def add_hotel(filename_hotel):
+    if os.path.exists(filename_hotel):
         try:
-            with open(filename, "a") as file:
+            with open(filename_hotel, "a") as file:
                 hotel = input("Enter the hotel name: ")
                 hotel_id = input("Enter the hotel id: ")
-                price = input("Enter the price/night (RM): ")
-                total_commission = 0.1 * float(price)
-                file.write(f"\n{hotel} | {hotel_id} | {price} | {total_commission}")
+                hotel_price = input("Enter the hotel_price/night (RM): ")
+                total_commission = 0.1 * float(hotel_price)
+                file.write(f"\n{hotel} | {hotel_id} | {hotel_price} | {total_commission}")
         except IOError as e:
             print(f"Error adding hotel: {e}")
     else:
-        print(f"File '{filename}' does not exist.")
+        print(f"File '{filename_hotel}' does not exist.")
 
-def read_file(filename):
-    if os.path.exists(filename):
+def read_file_hotel(filename_hotel):
+    if os.path.exists(filename_hotel):
         try:
-            with open(filename, "r") as file:
+            with open(filename_hotel, "r") as file:
+                header = f"\n{'ID':40}{'Hotel Name':>20}{'Price/Night':>20}{'Total Commission':>20}"
+                print(header)
+                print("=" * 100)
                 for line in file.readlines():
                     line = line.strip()
                     if line and "|" in line:
-                        hotel, hotel_id, price, total_commission = line.split("|")
-                        print(f"{hotel.strip():40}{hotel_id.strip():>20}{price.strip():>20}{total_commission.strip():>20}")
+                        hotel, hotel_id, hotel_price, total_commission = line.split("|")
+                        print(f"{hotel_id.strip():40}{hotel.strip():>20}{hotel_price.strip():>20}{total_commission.strip():>20}")
         except IOError as e:
             print(f"Error reading file: {e}")
     else:
-        print(f"File '{filename}' does not exist.")
+        print(f"File '{filename_hotel}' does not exist.")
 
-import os
 
-def edit_file(filename):
-    if os.path.exists(filename):
+def edit_file_hotel(filename_hotel):
+    if os.path.exists(filename_hotel):
         try:
-            with open(filename, "r+") as file:
+            with open(filename_hotel, "r+") as file:
                 lines = file.readlines()
                 file.seek(0)
 
@@ -53,8 +55,8 @@ def edit_file(filename):
                 for line_index, line in enumerate(lines):
                     line = line.strip()
                     if "|" in line:
-                        hotel, hotel_id, price, total = line.split("|")
-                        print(f"{hotel.strip():40}{hotel_id.strip():>20}{price.strip():>20}{total.strip():>20}")
+                        hotel, hotel_id, hotel_price, total = line.split("|")
+                        print(f"{hotel.strip():40}{hotel_id.strip():>20}{hotel_price.strip():>20}{total.strip():>20}")
 
                 search_hotel = input("Enter the hotel to edit or delete: ").strip().lower()
                 edited = False
@@ -65,7 +67,7 @@ def edit_file(filename):
                         if hotel.lower() == search_hotel:
                             print("\n1. Edit hotel")
                             print("2. Edit hotel_id")
-                            print("3. Edit price")
+                            print("3. Edit hotel_price")
                             print("4. Delete hotel")
                             print("5. Exit")
                             choice = input("Enter your choice: ")
@@ -83,14 +85,14 @@ def edit_file(filename):
                                 else:
                                     print("Invalid input. hotel_id must be a number.")
                             elif choice == "3":
-                                new_price = input("Enter the new price: ").strip()
+                                new_hotel_price = input("Enter the new hotel_price: ").strip()
                                 try:
-                                    float(new_price)
-                                    parts[2] = new_price
+                                    float(new_hotel_price)
+                                    parts[2] = new_hotel_price
                                     edited = True
-                                    print(f"Price '{price}' has been updated to '{new_price}'.")
+                                    print(f"hotel_Price '{hotel_price}' has been updated to '{new_hotel_price}'.")
                                 except ValueError:
-                                    print("Invalid input. Price must be a number.")
+                                    print("Invalid input. hotel_Price must be a number.")
                             elif choice == "4":
                                 confirmation = input(f"Are you sure you want to delete '{hotel}'? (y/n): ").strip().lower()
                                 if confirmation == "y":
@@ -119,61 +121,214 @@ def edit_file(filename):
         except IOError as e:
             print(f"Error editing file: {e}")
     else:
-        print(f"File '{filename}' does not exist.")
+        print(f"File '{filename_hotel}' does not exist.")
+
+#--------FOR TOUR -----------------
+def create_file_tour(filename_tour):
+    if not os.path.exists(filename_tour):
+        try:
+            with open(filename_tour, "x") as file:
+                file.write("tour | tour_id | tour_Price | Total Commission\n")
+        except IOError as e:
+            print(f"Error creating file: {e}")
+    else:
+        print(f"File '{filename_tour}' already exists.")
+
+def add_tour(filename_tour):
+    if os.path.exists(filename_tour):
+        try:
+            with open(filename_tour, "a") as file:
+                tour = input("Enter the tour name: ")
+                tour_id = input("Enter the tour id: ")
+                tour_price = input("Enter the tour price/night (RM): ")
+                total_commission = 0.1 * float(tour_price)
+                file.write(f"\n{tour} | {tour_id} | {tour_price} | {total_commission}")
+        except IOError as e:
+            print(f"Error adding tour: {e}")
+    else:
+        print(f"File '{filename_tour}' does not exist.")
+
+def read_file_tour(filename_tour):
+    if os.path.exists(filename_tour):
+        try:
+            with open(filename_tour, "r") as file:
+                # Print the header once
+                header = f"{'Tour Guide':40}{'ID':20}{'Price/Day':20}{'Total Commission':20}" 
+                print(header)
+                print("=" * 80)
+                for line in file.readlines():
+                    line = line.strip()
+                    if line and "|" in line:
+                        tour, tour_id, tour_price, total_commission = line.split("|")
+                        print(f"{tour.strip():40}{tour_id.strip():>20}{tour_price.strip():>20}{total_commission.strip():>20}")
+                        
+        except IOError as e:
+            print(f"Error reading file: {e}")
+    else:
+        print(f"File '{filename_tour}' does not exist.")
+
+
+def edit_file_tour(filename_tour):
+    if os.path.exists(filename_tour):
+        try:
+            with open(filename_tour, "r+") as file:
+                lines = file.readlines()
+                file.seek(0)
+
+                found = False
+                for line_index, line in enumerate(lines):
+                    line = line.strip()
+                    if "|" in line:
+                        tour, tour_id, tour_price, total = line.split("|")
+                        print(f"{tour.strip():40}{tour_id.strip():>20}{tour_price.strip():>20}{total.strip():>20}")
+
+                search_tour = input("Enter the tour to edit or delete: ").strip().lower()
+                edited = False
+                for line_index, line in enumerate(lines):
+                    if "|" in line:
+                        parts = line.strip().split("|")
+                        tour = parts[0].strip()
+                        if tour.lower() == search_tour:
+                            print("\n1. Edit tour")
+                            print("2. Edit tour_id")
+                            print("3. Edit tour_price")
+                            print("4. Delete tour")
+                            print("5. Exit")
+                            choice = input("Enter your choice: ")
+                            if choice == "1":
+                                new_tour = input("Enter the new tour: ").strip()
+                                parts[0] = new_tour
+                                edited = True
+                                print(f"tour '{tour}' has been updated to '{new_tour}'.")
+                            elif choice == "2":
+                                new_tour_id = input("Enter the new tour_id: ").strip()
+                                if new_tour_id.isdigit():
+                                    parts[1] = new_tour_id
+                                    edited = True
+                                    print(f"tour_id '{tour_id}' has been updated to '{new_tour_id}'.")
+                                else:
+                                    print("Invalid input. tour_id must be a number.")
+                            elif choice == "3":
+                                new_tour_price = input("Enter the new tour_price: ").strip()
+                                try:
+                                    float(new_tour_price)
+                                    parts[2] = new_tour_price
+                                    edited = True
+                                    print(f"tour_Price '{tour_price}' has been updated to '{new_tour_price}'.")
+                                except ValueError:
+                                    print("Invalid input. tour_Price must be a number.")
+                            elif choice == "4":
+                                confirmation = input(f"Are you sure you want to delete '{tour}'? (y/n): ").strip().lower()
+                                if confirmation == "y":
+                                    del lines[line_index]
+                                    edited = True
+                                    print(f"tour '{tour}' has been deleted.")
+                                    break  # Exit after deletion
+                                else:
+                                    print(f"Deletion of '{tour}' canceled.")
+                            elif choice == "5":
+                                print("Exiting...")
+                                break
+                            else:
+                                print("Invalid choice. Try again.")
+                                print("")
+                            if edited:
+                                lines[line_index] = " | ".join(parts) + "\n"
+                            break
+
+                if not edited:
+                    print(f"tour '{search_tour}' not found.")
+
+                file.seek(0)
+                file.writelines(lines)
+                file.truncate()
+        except IOError as e:
+            print(f"Error editing file: {e}")
+    else:
+        print(f"File '{filename_tour}' does not exist.")
 
 def main():
     while True:
-        print("1. Create new file")
-        print("2. Choose existing file")
+       
+        print("1. Open Hotel file")
+        print("2. Open Tour-Guide file")
         print("3. Exit")
         choice = input("Enter your choice: ")
 
         if choice == "1":
-            filename = input("Enter the filename to create: ")
-            create_file(filename)
+            filename_hotel = "hotel.txt"
             while True:
-                print("\nOperations:")
-                print("1. Add hotel")
-                print("2. Read file")
-                print("3. Edit file")
-                print("4. Back to main menu")
-                sub_choice = input("Enter your choice: ")
-
-                if sub_choice == "1":
-                    add_hotel(filename)
-                elif sub_choice == "2":
-                    read_file(filename)
-                elif sub_choice == "3":
-                    edit_file(filename)
-                elif sub_choice == "4":
-                    break
-                else:
-                    print("Invalid choice. Try again.")
-        
-        elif choice == "2":
-            filename = input("Enter the existing filename: ")
-            while True:
-                if os.path.exists(filename):
+                if os.path.exists(filename_hotel):
                     print("\nOperations:")
                     print("1. Add hotel")
-                    print("2. Read file")
+                    print("2. List all hotels")
                     print("3. Edit file")
                     print("4. Back to main menu")
-                    sub_choice = input("Enter your choice: ")
+                    sub_choice = input("Enter your choice \U0001F449  ")
 
                     if sub_choice == "1":
-                        add_hotel(filename)
+                        add_hotel(filename_hotel)
                     elif sub_choice == "2":
-                        read_file(filename)
+                        read_file_hotel(filename_hotel)
                     elif sub_choice == "3":
-                        edit_file(filename)
+                        edit_file_hotel(filename_hotel)
                     elif sub_choice == "4":
                         break
                     else:
                         print("Invalid choice. Try again.")
                 else:
-                    print(f"File '{filename}' does not exist.")
+                    print(f"File '{filename_hotel}' does not exist.")
                     break
+
+        elif choice == "2":
+            filename_tour = "tour.txt"
+            while True:
+
+                if os.path.exists(filename_tour):
+                    print("\nOperations:")
+                    print("1. Add tour-guide name")
+                    print("2. List all tour guides")
+                    print("3. Edit file")
+                    print("4. Back to main menu")
+                    sub_choice = input("Enter your choice \U0001F449  ")
+                    
+
+                    if sub_choice == "1":
+                        add_tour(filename_tour)
+                    elif sub_choice == "2":
+                        read_file_tour(filename_tour)
+                    elif sub_choice == "3":
+                        edit_file_tour(filename_tour)
+                    elif sub_choice == "4":
+                        break
+                    else:
+                        print("Invalid choice. Try again.")
+                else:
+                    print(f"File '{filename_tour}' does not exist.")
+                    break
+        #     filename_hotel = input("Enter the filename_hotel to create: ")
+        #     create_file(filename_hotel)
+        #     while True:
+        #         print("\nOperations:")
+        #         print("1. Add hotel")
+        #         print("2. Read file")
+        #         print("3. Edit file")
+        #         print("4. Back to main menu")
+        #         sub_choice = input("Enter your choice: ")
+
+        #         if sub_choice == "1":
+        #             add_hotel(filename_hotel)
+        #         elif sub_choice == "2":
+        #             read_file(filename_hotel)
+        #         elif sub_choice == "3":
+        #             edit_file(filename_hotel)
+        #         elif sub_choice == "4":
+        #             break
+        #         else:
+        #             print("Invalid choice. Try again.")
+        
+        
+            
         
         elif choice == "3":
             print("\nExiting...")
