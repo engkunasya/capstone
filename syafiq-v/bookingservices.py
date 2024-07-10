8bb75328135cea13f77779dad0aa136fcc7c1a0f
syafiqiiman98 <syafiqiiman98@gmail.com>
2024-07-10 14:34:30 +0800

    commit

--- syafiq-v/bookingservices.py
+++ syafiq-v/bookingservices.py
@@ -1,13 +1,13 @@
-def Titlename(filename):
-    with open(filename, "xt") as file:
-        file.write("BookingID | Category Service | Commision | Price/Day")
+# def Titlename(filename):
+#     with open(filename, "xt") as file:
+#         file.write("BookingID | Category Service | Commision | Price/Day")
 
 
-def Addaisyah(filename):
-    with open(filename, "r") as filehandler:
-        line = filehandler.readline()
-        with open()
+# def Addaisyah(filename):
+#     with open(filename, "r") as filehandler:
+#         line = filehandler.readline()
+#         with open()
 
 
 
-filename = "bookingservices.txt"
+# filename = "bookingservices.txt"
